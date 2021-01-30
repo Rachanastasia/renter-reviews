baa8086eb99eaa825897df4d058c137d6bb45e38
Rachel Reilly <r.a.reill18@gmail.com>
2021-01-30 13:39:27 -0600

    user and token services built & handleLogin handleLogout funcs in context

--- contexts/userContext.js
+++ contexts/userContext.js
@@ -1,12 +1,41 @@
 import React, { createContext, useState } from 'react'
-
+import TokenService from '../services/token-service.js'
+import { postLogin, postNewUser } from './services/user-auth-service.js'
 export const UserContext = createContext()
 
 function UserContextProvider({ children }) {
-  const [user, setUser] = useState('testing')
+  const [user, setUser] = useState({})
   const [error, setError] = useState(null)
 
-  const value = { user, error }
+  const handleLogin = async (email, password) => {
+    try {
+      const authToken = await postLogin(email, password)
+
+      if (!authToken) {
+        setError(authToken.error)
+        return
+      }
+
+      await TokenService.saveAuthToken(authToken)
+      const { email, id, nickname } = TokenService.parseAuthToken()
+      setUser({
+        id,
+        nickname,
+        email
+      })
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleLogout = () => {
+    TokenService.clearAuthToken()
+    setUser({})
+  }
+
+
+  const value = { user, error, setError, handleLogin, handleLogout }
   return (
     <UserContext.Provider value={value}>
       {children}
---
+++ services/user-auth-service.js
@@ -0,0 +1,23 @@
+import { REACT_APP_API_ENDPOINT } from './config'
+
+const UserAuthService = {
+  postLogin(email, password) {
+    return fetch(`${REACT_APP_API_ENDPOINT}/auth/login`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    })
+      .then(res => (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json())
+  },
+  postNewUser(email, password, nickname) {
+    return fetch(`${REACT_APP_API_ENDPOINT}/user`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ email, password, nickname })
+    })
+  }
+}
+
+export default UserAuthService
